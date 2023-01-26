6125fcd87a8eb3d19cba451fe3d982acdd0c352c
Areeb29 <samitariq40@gmail.com>
2023-01-26 16:52:51 +0500

    api created and connected with lambda function

--- Backend/lib/backend-stack.ts
+++ Backend/lib/backend-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BackendStack extends cdk.Stack {
@@ -8,9 +10,18 @@ export class BackendStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create a new Lambda function
+    const myLambdaFunction = new lambda.Function(this, 'MyLambdaFunction', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda/code'),
+      handler: 'index.handler',
+    });
+
+    // Create an API Gateway to trigger the Lambda function
+    const myApi = new apigw.RestApi(this, 'MyApi', {
+      restApiName: 'My API',
+    });
+    const myIntegration = new apigw.LambdaIntegration(myLambdaFunction);
+    myApi.root.addMethod('POST', myIntegration);
   }
 }
